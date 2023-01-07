4b260e275635afc86b6caff4e9d69734d5aa508c
Malika elha <elhamalika@gmail.com>
2023-01-07 21:43:04 +0100

    superposition images dans pages d'acceuil

---
+++ theme-planty/debut-accueil.php
@@ -0,0 +1,18 @@
+<!--Section permettant de composer l'image centrale de la page d'accueil en superposant 3 images-->
+<section class="fond radius">
+
+      <p class="green titleA">BOISSON ÉNERGISANTE <BR/> 100% NATURELLE</p>
+
+      <article class="img-accueil">
+        
+         <div class="parent">       
+         
+            <img src="http://planty.local/wp-content/uploads/2022/12/Group-4.png" alt="img1" class="superpose img1"/>
+            <img src="http://planty.local/wp-content/uploads/2022/12/Planty6-1-1.png" alt="img2" class="superpose img2"/>
+            <img src="http://planty.local/wp-content/uploads/2022/12/Group-3.png" alt="img3" class="superpose img3"/> 
+       
+        </div>
+       
+      </article>
+
+</section>
--- theme-planty/index.php
+++ theme-planty/index.php
@@ -2,6 +2,12 @@
 
 get_header();
 
+//intégrer le début de la page d'accueil
+$current_url = home_url($_SERVER['REQUEST_URI']);
+if ($current_url=="http://planty.local/"){
+    require_once("debut-accueil.php");
+}
+
 the_content();
 
 get_footer();
