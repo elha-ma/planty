062daae9ea2606ff2cd9bb3890f0fc8b54ca1e6e
Malika elha <elhamalika@gmail.com>
2023-01-12 16:58:28 +0100

    développement responsive pas d'accueil

--- theme-planty/debut-accueil.php
+++ theme-planty/debut-accueil.php
@@ -7,9 +7,9 @@
         
          <div class="parent">       
          
-            <img src="http://planty.local/wp-content/uploads/2022/12/Group-4.png" alt="img1" class="superpose img1"/>
-            <img src="http://planty.local/wp-content/uploads/2022/12/Planty6-1-1.png" alt="img2" class="superpose img2"/>
-            <img src="http://planty.local/wp-content/uploads/2022/12/Group-3.png" alt="img3" class="superpose img3"/> 
+            <img src="http://planty.local/wp-content/uploads/2022/12/Group-4.png" alt="BrancheGauche" class="superpose img1"/>
+            <img src="http://planty.local/wp-content/uploads/2022/12/Planty6-1-1.png" alt="BoissonPlanty" class="superpose img2"/>
+            <img src="http://planty.local/wp-content/uploads/2022/12/Group-3.png" alt="BrancheDroite" class="superpose img3"/> 
        
         </div>
        
