371c1a6ee649f2b39fa5e6dd8fd97d60c684d34c
Malika elha <elhamalika@gmail.com>
2023-01-17 12:06:58 +0100

    suite responsive

--- theme-planty/debut-accueil.php
+++ theme-planty/debut-accueil.php
@@ -7,12 +7,12 @@
         
          <div class="parent">       
          
-            <img src="http://planty.local/wp-content/uploads/2022/12/Group-4.png" alt="BrancheGauche" class="superpose img1"/>
-            <img src="http://planty.local/wp-content/uploads/2022/12/Planty6-1-1.png" alt="BoissonPlanty" class="superpose img2"/>
-            <img src="http://planty.local/wp-content/uploads/2022/12/Group-3.png" alt="BrancheDroite" class="superpose img3"/> 
+           <img src="http://planty.local/wp-content/uploads/2022/12/Group-4.png" alt="BrancheGauche" class="superpose img1"/>
+           <img src="http://planty.local/wp-content/uploads/2022/12/Planty6-1-1.png" alt="BoissonPlanty" class="superpose img2"/>
+           <img src="http://planty.local/wp-content/uploads/2022/12/Group-3.png" alt="BrancheDroite" class="superpose img3"/>
        
         </div>
-       
+      
       </article>
 
 </section>
